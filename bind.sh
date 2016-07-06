e6f25a4d7ea778236addfe101fcaae4b1be4601e
Kenneth Peiruza <kenneth@floss.cat>
2016-07-06 18:31:33 +0200

    Fixed, no way to implement TSIG for a network :'(

--- bind.sh
+++ bind.sh
@@ -4,39 +4,6 @@ configCore="/etc/bind/named.conf"
 #	Where to write TSIG and zones configuration
 configCustom="/etc/bind/named.conf.local"
 
-function initTSIG {
-	echo "Creating TSIG Configuration"
-#	ACL defining ranges where we want to use our TSIG key
-#	We define 172.16.0.0/12; here as all IP's assigned by default
-#	by docker will probably be inside 172.17.X.Y or 172.18.X.Y
-	echo 'acl remotepeers {
-	172.16.0.0/12;' >> $configCustom
-#	Additional IPs just in case they were defined
-	if [ -n "$BIND_MASTER_IP" ]
-	then
-		echo "$BIND_MASTER_IP;" >> $configCustom
-	fi
-	if [ -n "$BIND_SLAVE_IP" ]
-	then
-		echo "$BIND_SLAVE_IP;" >> $configCustom
-	fi
-#	End of TSIG stuff after next echo
-	echo '
-};
-
-key "interconecta" {
-	algorithm hmac-sha512;
-	secret "xN4RtMWnEq6/nsyfvZZ2E8zfi2jYqRjnQhhKUe1vPQgAExlYYNMDkABtAIGZt/51TKbY0Wwjld97VdbNRcIUAg==";
-};
-
-server remotepeers {
-	keys { interconecta; };
-};
-
-' | sed -e "s/BIND_SLAVE_IP;/$BIND_SLAVE_IP;/g" -e "s/BIND_MASTER_IP;/$BIND_MASTER_IP;/g" | grep -v "^\t;$" >> $configCustom
-
-}
-
 function initMasterZones {
 
 	echo "Creating Master Zone Configuration"
@@ -52,8 +19,8 @@ function initMasterZones {
 #	IP AUTH MODE
 				echo -e "zone \"$domain\" {\n\ttype master;\n\tfile \"/etc/bind/zones/$domain\";\n\tallow-transfer { $BIND_SLAVE_IP; };\n};\n\n" >> $configCustom
 			else
-#	TSIG AUTH MODE
-				echo -e "zone \"$domain\" {\n\ttype master;\n\tfile \"/etc/bind/zones/$domain\";\n\tallow-transfer { key interconecta; };\n};\n\n" >> $configCustom
+#	Defaults to docker0. We don't want everyone to be able to perform AXFR!
+				echo -e "zone \"$domain\" {\n\ttype master;\n\tfile \"/etc/bind/zones/$domain\";\n\tallow-transfer { 172.16.0.0/12; };\n};\n\n" >> $configCustom
 			fi
 		fi
 	done
@@ -72,11 +39,9 @@ function initSlaveZones {
 
 if [ "$BIND_MASTER" = "true" ]
 then
-	initTSIG
 	initMasterZones
 elif [ -n "$BIND_MASTER_IP" ]
 then
-	initTSIG
 	initSlaveZones
 else
 	echo "Missing required variables: BIND_MASTER==true+BIND_SLAVE_IP for master or BIND_MASTER_IP for slave"
